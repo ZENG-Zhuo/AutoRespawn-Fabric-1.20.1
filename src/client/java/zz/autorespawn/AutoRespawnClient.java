bfc05ca9c8c62a5aadc74bdd836d81d6bab574c6
Zhuo ZENG <zzengan@connect.ust.hk>
2024-12-12 21:29:05 -0500

    first commit

---
+++ src/client/java/zz/autorespawn/AutoRespawnClient.java
@@ -0,0 +1,10 @@
+package zz.autorespawn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoRespawnClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
